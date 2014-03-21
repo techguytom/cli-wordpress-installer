b058032d0cdf950f4cee62b9bab5b5055067fac5
Tom Jenkins <tjenkins@nerdery.com>
2014-03-21 18:05:45 -0500

    Start install test

---
+++ Installer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Techguytom\CliWordpressInstaller;
+
+use Symfony\Component\Yaml\Parser;
+
+class Installer {
+    public static function cliInstall()
+    {
+        var_dump('test');
+    }
+}
